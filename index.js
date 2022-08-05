6f5135eb5cfae34976d5fcc5b7394f76be7ad647
Md Habibur Rahman <shakibdshy@gmail.com>
2022-08-05 13:03:58 +0600

    update index file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO);
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Connected to MongoDB")
     } catch (error) {
         throw error
